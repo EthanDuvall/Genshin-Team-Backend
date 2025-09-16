c573454755de8bf24bc851f2431a41beed5e9ca2
Ethan <eduvall23@turing.edu>
2025-09-16 10:29:50 -0600

    create java app

--- README.md
+++
@@ -1,2 +0,0 @@
-# Genshin-Team-Backend
-Back end of my genshin team build to hoast the api
---
+++ src/test/java/genshinteambuillder/genshinTeamBuilderApi/GenshinTeamBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package genshinteambuillder.genshinTeamBuilderApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinTeamBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
